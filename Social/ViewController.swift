0fbbf98c3a68211633510624fc38359f49def397
John Crossley <jonnysnip3r@gmail.com>
2018-09-07 13:10:31 +0100

    initial commit with empty project

---
+++ Social/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Social
+//
+//  Created by John Crossley on 07/09/2018.
+//  Copyright © 2018 John Crossley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SocialTests/SocialTests.swift
@@ -0,0 +1,34 @@
+//
+//  SocialTests.swift
+//  SocialTests
+//
+//  Created by John Crossley on 07/09/2018.
+//  Copyright © 2018 John Crossley. All rights reserved.
+//
+
+import XCTest
+@testable import Social
+
+class SocialTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
